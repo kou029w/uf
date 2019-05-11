22694d07c6adc80614725b4022c55773d4a05a85
Kohei Watanabe <kou029w@gmail.com>
2019-05-11 12:02:56 +0900

    ほぼjs化

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 import { html, render } from "https://unpkg.com/lit-html?module";
 
-Object.entries({
+Object.assign(document.documentElement, {
   lang: "ja",
   dir: "ltr"
-}).forEach(([k, v]) => {
-  document.documentElement[k] = v;
 });
-
-document.title = "main.jsによって書き込まれているページ";
 
 render(
   html`
-    <h1>${document.title}</h1>
-    <p>
-      <code>main.js</code>は<a href=${import.meta.url}
-        ><code>${import.meta.url}</code></a
-      >に存在します。
-    </p>
+    <title>main.jsによって書き込まれているページ</title>
+    <meta
+      name="viewport"
+      content="width=device-width,minimum-scale=1,initial-scale=1"
+    />
+    <link rel="icon" href="#" />
   `,
-  document.body
+  document.head
 );
+
+setInterval(() => {
+  render(
+    html`
+      <h1>${document.title}</h1>
+      <p>
+        <code>main.js</code>は<a href=${import.meta.url}
+          ><code>${import.meta.url}</code></a
+        >に存在します。
+      </p>
+      <p>
+        現在時刻は${new Date().toLocaleString()}です。
+      </p>
+    `,
+    document.body
+  );
+});
