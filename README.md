92cfff53644ee2e10c04446b2daa06cdca36fa61
Kohei Watanabe <kou029w@gmail.com>
2019-08-22 22:17:48 +0900

    描画プロセスの変更

---
+++ README.md
@@ -0,0 +1,11 @@
+# uf
+
+最小限のコードで書かれた実験的な Web ページ
+
+以下の外部リソースに依存
+
+- unpkg.com
+  - lit-html
+- cdnjs.com
+  - github-markdown-css
+  - markdown-it
--- main.js
+++ main.js
@@ -1,40 +1,55 @@
-import { html, render } from "https://unpkg.com/lit-html?module";
-
-Object.assign(document.documentElement, {
-  lang: "ja",
-  dir: "ltr"
+import {
+  html,
+  render as litRender
+} from "https://unpkg.com/lit-html@1.1.2/lit-html.js?module";
+import "https://cdnjs.cloudflare.com/ajax/libs/markdown-it/9.1.0/markdown-it.min.js";
+const { url } = import.meta;
+const lang = "ja";
+const dir = "ltr";
+const head = () => html`
+  <title>uf</title>
+  <meta name="viewport" content="width=device-width" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css"
+  />
+`;
+const jsLink = html`
+  <a href=${url}>${url}</a>
+`;
+const body = ({ date, readme }) => html`
+  <main class="markdown-body">
+    ${readme}
+    <section>
+      <h2>JavaScriptによって書き込まれているセクション</h2>
+      <p>${jsLink}によって描画されています。</p>
+      <p>
+        現在時刻は${date}です。
+      </p>
+    </section>
+  </main>
+`;
+const md = window.markdownit();
+const fetchReadme = async () => {
+  const res = await fetch("README.md");
+  const text = await res.text();
+  return html([md.render(text)]);
+};
+const state = {
+  readme: undefined,
+  date: new Date().toLocaleString()
+};
+const render = () => {
+  Object.assign(document.documentElement, { lang, dir });
+  litRender(head(), document.head);
+  litRender(body(state), document.body);
+};
+fetchReadme().then(html => {
+  state.readme = html;
+  render();
 });
-
-render(
-  html`
-    <title>main.jsによって書き込まれているページ</title>
-    <meta
-      name="viewport"
-      content="width=device-width,minimum-scale=1,initial-scale=1"
-    />
-    <link rel="icon" href="#" />
-  `,
-  document.head
-);
-
 setInterval(() => {
-  render(
-    html`
-      <style>
-        @import "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css";
-      </style>
-      <article class="markdown-body">
-        <h1>${document.title}</h1>
-        <p>
-          <code>main.js</code>は<a href=${import.meta.url}
-            ><code>${import.meta.url}</code></a
-          >に存在します。
-        </p>
-        <p>
-          現在時刻は${new Date().toLocaleString()}です。
-        </p>
-      </article>
-    `,
-    document.body
-  );
-});
+  state.date = new Date().toLocaleString();
+  render();
+}, 1e3);
+render();
