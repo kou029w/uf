a297ed45bb593353a06c27ee3b07f84fbce0af54
Kohei Watanabe <kou029w@gmail.com>
2020-06-13 21:50:15 +0900

    refactor

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ import {
   render as litRender,
 } from "https://unpkg.com/lit-html@1.1.2/lit-html.js?module";
 import "https://cdn.jsdelivr.net/npm/markdown-it@11.0.0/dist/markdown-it.min.js";
-const { url } = import.meta;
-const lang = "ja";
-const dir = "ltr";
 const head = () => html`
   <title>uf</title>
   <meta name="viewport" content="width=device-width" />
@@ -14,40 +11,42 @@ const head = () => html`
     href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
   />
 `;
-const jsLink = html` <a href=${url}>${url}</a> `;
 const body = ({ date, readme }) => html`
   <main>
     ${readme}
     <section>
       <h2>JavaScriptによって書き込まれているセクション</h2>
-      <p>${jsLink}によって描画されています。</p>
+      <p>
+        <a href=${import.meta.url}>${import.meta.url}</a>
+        によって描画されています。
+      </p>
       <p>
         現在時刻は${date}です。
       </p>
     </section>
   </main>
 `;
-const md = window.markdownit();
-const fetchReadme = async () => {
+async function fetchReadme() {
   const res = await fetch("README.md");
   const text = await res.text();
-  return html([md.render(text)]);
-};
-const state = {
-  readme: undefined,
-  date: new Date().toLocaleString(),
-};
-const render = () => {
-  Object.assign(document.documentElement, { lang, dir });
+  return html([window.markdownit().render(text)]);
+}
+function render(state) {
+  Object.assign(document.documentElement, { lang: "ja", dir: "ltr" });
   litRender(head(), document.head);
   litRender(body(state), document.body);
-};
-fetchReadme().then((html) => {
-  state.readme = html;
-  render();
-});
-setInterval(() => {
-  state.date = new Date().toLocaleString();
-  render();
-}, 1e3);
-render();
+}
+async function main() {
+  const state = {
+    readme: undefined,
+    date: new Date().toLocaleString(),
+  };
+  setInterval(() => {
+    state.date = new Date().toLocaleString();
+    render(state);
+  }, 1e3);
+  render(state);
+  state.readme = await fetchReadme();
+  render(state);
+}
+main();
